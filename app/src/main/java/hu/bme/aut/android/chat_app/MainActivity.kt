62b527dec9fad4ff5c971b8ed9e1dd65a20ca0d7
LizaSzabo <47565900+LizaSzabo@users.noreply.github.com>
2021-05-16 01:03:08 +0200

    ChatBox as app name

--- app/src/main/java/hu/bme/aut/android/chat_app/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/android/chat_app/MainActivity.kt
@@ -1,33 +1,22 @@
 package hu.bme.aut.android.chat_app
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
+
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Base64
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentManager
 import com.amplifyframework.core.Amplify
-import hu.bme.aut.android.chat_app.ChatApplication.Companion.allConversationList
 import hu.bme.aut.android.chat_app.ChatApplication.Companion.messageText
-import hu.bme.aut.android.chat_app.ChatApplication.Companion.usersList
-import hu.bme.aut.android.chat_app.Model.Conversation
-import hu.bme.aut.android.chat_app.Network.initializeUserData
 import hu.bme.aut.android.chat_app.Network.querys
 import hu.bme.aut.android.chat_app.databinding.ActivityMainBinding
-import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    lateinit var fragmentmanager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
